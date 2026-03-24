502548cf24b795adaeee2e29ee666d6bafbdacae
Balamurugan66-code <balamurugan99451@gmail.com>
2026-03-24 21:28:11 +0530

    fixed cors and cookies

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-voice-assistant-va.onrender.com",
     credentials:true
 }))
 const PORT=process.env.PORT || 8000
